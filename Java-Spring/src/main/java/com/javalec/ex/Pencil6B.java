997f1754db736f0bba06e5c85f031a8d155bb167
ChangWoo Choi <rvege1409@gmail.com>
2018-12-06 17:25:27 +0900

    initial commit

---
+++ Java-Spring/src/main/java/com/javalec/ex/Pencil6B.java
@@ -0,0 +1,12 @@
+package com.javalec.ex;
+
+public class Pencil6B implements Pencil {
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		System.out.println("6B입니다.");
+
+	}
+
+}
