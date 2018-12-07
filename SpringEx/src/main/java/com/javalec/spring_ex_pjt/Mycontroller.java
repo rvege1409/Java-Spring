16350f162466fffd07b2ea0f88e2c403aa8a9443
ChangWoo Choi <rvege1409@gmail.com>
2018-12-07 14:13:29 +0900

    start ~ model and View

---
+++ SpringEx/src/main/java/com/javalec/spring_ex_pjt/Mycontroller.java
@@ -0,0 +1,42 @@
+package com.javalec.spring_ex_pjt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Mycontroller {
+	
+	@RequestMapping("/view")
+	public String view() {
+		return "view";
+	}
+	
+	@RequestMapping("/content/contentView")
+	public String contentView(Model model) {
+		
+		model.addAttribute("id","abcde");
+		return "content/contentView";
+	}
+	
+	@RequestMapping("/model/modelEx")
+	public String modelEx(Model model) {
+		
+		model.addAttribute("data", "abcedfgh");
+		
+		
+		return "/model/modelEx";
+	}
+	
+	@RequestMapping("/modelAndView/modelView")
+	public ModelAndView modelAndView() {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("id","abcde");
+		mv.setViewName("/modelAndView/modelView");
+		
+		
+		return mv;
+	}
+}
---
+++ SpringEx/src/main/webapp/WEB-INF/views/modelAndView/modelView.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+modelView.jsp 입니다.
+
+id :${id}
+</body>
+</html>
